f62f62cc72ef07a23401e245ec2c539829675e5c
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-01-24 15:47:29 -0600

    leaf node class added and tested using test cases

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -27,3 +27,30 @@ class HTMLNode():
         return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
 
 
+#Class to generate a LeafNode - single HTML tag with no children.
+class LeafNode(HTMLNode):
+
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+        self.tag = tag
+        self.value = value
+        self.props = props
+
+    def to_html(self):
+        
+        if self.value is None:
+            raise ValueError
+        
+        if self.tag is None:
+            return self.value
+        
+        else:
+            if self.props:
+                formatted_props_to_html = self.props_to_html()
+                
+                return f'<{self.tag}{formatted_props_to_html}>{self.value}</{self.tag}>'
+            else:
+                return f'<{self.tag}>{self.value}</{self.tag}>'
+            
+    def __repr__(self):
+        return f'LeafNode({self.tag}, {self.value}, {self.props})'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,7 @@
 #Unit tests to validate htmlnodes
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_empty(self):
@@ -15,6 +15,14 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode(tag='p', value='Testing of HTML nodes', props={"href": "www.youtube.com", "title": "davinci resolve"})
         self.assertEqual(node.props_to_html(), f' href="www.youtube.com" title="davinci resolve"')
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode("a", "Click me!", props={"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
 
 
 if __name__ == "__main__":
