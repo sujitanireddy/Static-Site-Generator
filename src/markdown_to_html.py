d906463ea04071e7eb9fdf4738c61ee50d12ce78
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-02-15 16:13:53 -0600

    In markdown_to_html_node function: Quotes  are handeled

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -28,9 +28,7 @@ def markdown_to_html_node(markdown):
             collection.append(ParentNode(children=text_to_children(heading_text), tag=f"h{heading_size}"))
 
         if block_type == BlockType.PARAGRAPH:
-            block_to_line = ""
-            for line in block.split("\n"):
-                block_to_line += f" {line}".strip()
+            block_to_line = " ".join(block.split("\n"))
             collection.append(ParentNode(children=text_to_children(block_to_line), tag="p"))
 
         if block_type == BlockType.ULIST:
@@ -43,9 +41,18 @@ def markdown_to_html_node(markdown):
         if block_type == BlockType.OLIST:
             li_nodes = []
             for line in block.split("\n"):
-                line_text = line[1:].strip()
+                for i, char in enumerate(line):
+                    if char == ' ':
+                        break
+                line_text = line[i:].strip()
                 li_nodes.append(ParentNode(children=text_to_children(line_text), tag="li"))
             collection.append(ParentNode(children=li_nodes, tag="ol"))
+
+        if block_type == BlockType.QUOTE:
+            quote_text = " ".join([quote[1:] for quote in block.split("\n")])
+            collection.append(ParentNode(children=text_to_children(quote_text), tag="blockquote"))
+        
+    return ParentNode(children=collection, tag="div")
 
                 
 
